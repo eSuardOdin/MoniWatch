6f495080281a51a644908ca69dc632a8e9007de1
eSuardOdin <esuardodin@gmail.com>
2023-08-24 05:03:44 +0200

    Http GET moni now needs a password to be tested against hashed version

--- MoniWatch.Api/Controllers/MoniController.cs
+++ MoniWatch.Api/Controllers/MoniController.cs
@@ -18,13 +18,23 @@ public class MoniController : ControllerBase
         _logger = logger;
     }
 
+
+    /// <summary>
+    /// Returns a Moni with matching login and password
+    /// </summary>
+    /// <param name="moniLogin">The Login to search in db</param>
+    /// <param name="moniPwd">Password that will be tested against hashed password in db</param>
+    /// <returns>Not found or moni object</returns>
     [HttpGet(Name="GetMoni")]
-    public async Task<ActionResult<Moni>> GetMoni(string moniLogin)
+    public async Task<ActionResult<Moni>> GetMoni(string moniLogin, string moniPwd)
     {
         using (MoniWatchDbContext db = new())
         {
-            Moni moni = await db.Monies.Where(m => m.MoniLogin == moniLogin).FirstOrDefaultAsync();
-            if (moni is null)
+            Moni moni = await db.Monies.Where(
+                m => m.MoniLogin == moniLogin)
+                .FirstOrDefaultAsync();
+
+            if (moni is null || !BcryptNet.Verify(moniPwd, moni.MoniPwd))
             {
                 return NotFound();
             }
@@ -35,19 +45,27 @@ public class MoniController : ControllerBase
         }
     }
 
-
+    /// <summary>
+    /// Adds a Moni to database with an hashed password
+    /// </summary>
+    /// <param name="moni">Moni object added, will be passed in request as JSON</param>
+    /// <returns>The Moni created</returns>
     [HttpPost(Name="PostMoni")]
-    //public void PostMoni([FromBody] Moni moni)
-    public async Task<ActionResult<Moni>> PostMoni([FromBody] Moni moni)
+    public void PostMoni([FromBody] Moni moni)
+    // public async Task<ActionResult<Moni>> PostMoni([FromBody] Moni moni)
     {
         // Pwd encrypt
-        moni.MoniPwd = BcryptNet.HashPassword(moni.MoniPwd);
-        /* Console.WriteLine($"Pass : {moni.MoniPwd}");
+        // moni.MoniPwd = BcryptNet.HashPassword(moni.MoniPwd);
+
+
+
+        string hashed = BcryptNet.HashPassword(moni.MoniPwd);
+        Console.WriteLine($"Pass : {moni.MoniPwd}");
         Console.WriteLine($"Crypted : {hashed}");
         Console.WriteLine(BcryptNet.Verify(moni.MoniPwd, "$2a$11$d/eripQMuqiA5zkzA3ho7uIq2x6gWq4kykhKglmV3xZ6KJpGkesLC"));
         Console.WriteLine(BcryptNet.Verify(moni.MoniPwd, hashed));
-        */
-        using (MoniWatchDbContext db = new())
+       
+        /* using (MoniWatchDbContext db = new())
         {
             if(moni is null)
             {
@@ -58,6 +76,6 @@ public class MoniController : ControllerBase
 
             return CreatedAtAction(nameof(GetMoni), new {moniLogin = moni.MoniLogin}, moni);
 
-        } 
+        }  */
     }
 }
