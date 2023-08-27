29060acad0bbe6148c7f57dc9a34b7ff9881c6bb
eSuardOdin <esuardodin@gmail.com>
2023-08-27 18:37:59 +0200

    Deleted a value in db, seems to happen after the GetAll even if I put it before, need to ask about async

--- ClientApp/ConsoleClient/Program.cs
+++ ClientApp/ConsoleClient/Program.cs
@@ -8,10 +8,11 @@ HttpClient client = new HttpClient();
 try
 {
     // using HttpResponseMessage res = await client.GetAsync("http://localhost:5000/transaction/GetAllTransactions");
-    var transactions = await client.GetFromJsonAsync<List<Transaction>>("http://localhost:5000/transaction/GetAllTransactions");
+    var transactions = await client.GetFromJsonAsync<List<Transaction>>("http://192.168.30.10:5000/transaction/GetAllTransactions");
+    // await client.DeleteAsync("http://192.168.30.10:5000/transaction/DeleteTransaction?transactionId=2");
     foreach(Transaction tr in transactions)
     {
-        Console.WriteLine($"{tr.TransactionName}: {tr.TransactionAmount}€");
+        Console.WriteLine($"ID: {tr.TransactionId} -> {tr.TransactionName}: {tr.TransactionAmount}€");
     }
     // Console.WriteLine(result);
 }
