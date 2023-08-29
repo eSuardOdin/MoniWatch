3624e19e31c89f4fe45dd253a0dc5cea09fa9930
eSuardOdin <esuardodin@gmail.com>
2023-08-30 00:43:10 +0200

    Trying to add a text input event, failed for now

--- ClientApp/AvaloniaTests/Avaloniapp/MainWindow.axaml.cs
+++ ClientApp/AvaloniaTests/Avaloniapp/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using System.Diagnostics;
 using System;
@@ -14,6 +15,32 @@ public partial class MainWindow : Window
 
     public void ButtonClicked (object source, RoutedEventArgs args)
     {
-        Console.WriteLine("Click");
+        if (double.TryParse(textBoxCelsius.Text, out var celsius))
+        {
+            double f = celsius * (9d/5d) + 32;
+            textBoxFahrenheit.Text = f.ToString("0.00");
+        }
+        else
+        {
+            Console.WriteLine("Error in data submission");
+            textBoxCelsius.Text = "0";
+            textBoxFahrenheit.Text = "0";
+        }
+    }
+
+    public void InputEntered(object source, TextInputEventArgs args)
+    {
+        Console.WriteLine("Entered");
+        if (double.TryParse(textBoxCelsius.Text, out var celsius))
+        {
+            double f = celsius * (9d/5d) + 32;
+            textBoxFahrenheit.Text = f.ToString("0.00");
+        }
+        else
+        {
+            Console.WriteLine("Error in data submission");
+            textBoxCelsius.Text = "0";
+            textBoxFahrenheit.Text = "0";
+        } 
     }
 }
